b69d65b1e9cf5e00243cd8fc85e90ace470cea10
BryanCox <bryanc71@yahoo.com>
2018-11-12 14:22:33 -0700

    tweak

--- controllers/template.js
+++ controllers/template.js
@@ -174,8 +174,10 @@ register: function(req, res) {
 
   information_search: function(req, res) {
     var searchText = req.body.Title.replace(/\b[a-z]/g,function(f){return f.toUpperCase()})
-    var queryBuilder = knex.select('*').from('glossary') 
-    queryBuilder.where('Title', 'like', `%${searchText}%`)
+    var queryBuilder = knex.select('*').from('glossary').rightJoin('case_files', 'glossary.id', 'case_files.id')
+    queryBuilder.where('Title', 'like', `%${searchText}%`).orWhere('First_Name', 'like', `%${searchText}%`).orWhere('Last_Name', 'like', `%${searchText}%`)
+    // .then(
+    // knex.select('*').from('case_files').where('First_Name', 'like', `%${searchText}%`).orWhere('Last_Name', 'like', `%${searchText}%`)
       .then( query => 
         res.json(query))  
   },
